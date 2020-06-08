79dc8012ceea1bc00ac989be8cb9386929931493
tannerb9 <tanner-brainard@hotmail.com>
2020-06-08 13:18:12 -0500

    created Login comp

---
+++ src/components/auth/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { handleFieldChange } from "../../helpers/Helpers";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  const handleLogin = (props) => {
+    evt.preventDefault();
+    sessionStorage.setItem("credentials", JSON.stringify(credentials));
+    props.history.push("/");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        <fieldset>
+          <div className="formgrid">
+            <input
+              type="text"
+              type="email"
+              onChange={(evt) =>
+                handleFieldChange(evt, credentials, setCredentials)
+              }
+              required
+              placeholder="E-mail address"
+              autoFocus=""
+            />
+            <label htmlFor="inputEmail">Email address</label>
+            <input
+              type="password"
+              onChange={(evt) =>
+                handleFieldChange(evt, credentials, setCredentials)
+              }
+              id="password"
+              placeholder="Password"
+              required
+            />
+            <label htmlFor="inputPassword">Password</label>
+          </div>
+          <button type="submit">Sign in</button>
+        </fieldset>
+      </form>
+    </>
+  );
+};
+
+export default Login;
